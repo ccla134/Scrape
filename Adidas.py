6eebeffb400388a9c658ca3a7d519515f1924d81
ccla134 <ccla134@lsu.edu>
2023-06-28 12:25:29 -0500

    Selenium fixes, might need to change the chrome driver

--- Adidas.py
+++ Adidas.py
@@ -1,3 +1,4 @@
+
 import time
 import requests
 from bs4 import BeautifulSoup
@@ -9,13 +10,51 @@ from webdriver_manager.chrome import ChromeDriverManager
 import selenium.common.exceptions as exceptions
 import gspread
 
-url = 'https://www.adidas.com/us/women-basketball-shoes'
-html = requests.get(url)
+urls = ['https://www.adidas.com/us/women-basketball-shoes',
+        'https://www.adidas.com/us/women-basketball-shoes?start=48',
+        'https://www.adidas.com/us/women-basketball-shoes?start=96']
+
+# headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+# response = requests.get(url, headers=headers)
+
+# 
+
+# result = s.find('div', class_='container')
+# # #urls = result.find_all('a')
+
+# print(s)
+
+global driver
+options = Options()
+options.add_argument("start-maximized")
+# options.headless = True
+options.add_experimental_option("excludeSwitches", ["enable-automation"])
+options.add_experimental_option("detach", True)
+options.add_experimental_option('useAutomationExtension', False)
+
+page_source = ['', '', '']
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+for i in range(0, len(urls)):
+    driver.get(urls[i])
+    page_source[i] = driver.page_source
+
+for i in range (0, len(page_source)):
+    s = BeautifulSoup(page_source[i], 'html.parser')
+    result = s.find('div', class_='product-container___3GvlZ')
+    cards = result.find_all('div', class_='glass-product-card__assets')
+
+    for j in range(0, len(cards)):
+        url = cards[j].find('a')
+        if url['href'].split('/')[1] == 'us':
+            url = 'https://www.adidas.com/' + url['href']
+        else:
+            url = url['href']
+        driver.get(url)
+        dump = driver.page_source
+        d = BeautifulSoup(dump, 'html.parser')
+        title = d.find('h1', class_='name___120FN')
+        print(title, url)
+driver.close()
 
 
-s = BeautifulSoup(html.content, 'html.parser')
-
-result = s.find('div', class_='plp-grid__1FP1J')
-urls = result.find_all('a')
-
-print(urls)
